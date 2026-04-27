bd43a3523d6babe056e7f0a21552ca0564e5ed20
Nick <109121197+nicholasPenaranda@users.noreply.github.com>
2026-04-26 22:48:45 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-//get with job regarding setting up a button for apartments
+const complaint = document.querySelector("#complaint");
+const connectedAp = document.querySelector("#connectedAp");
+const operatorId = document.querySelector("#operatorId");
+const sinrDown = document.querySelector("#sinrDown");
+const sinrUp = document.querySelector("#sinrUp");
+const alignmentMetric = document.querySelector("#alignmentMetric");
+const pathloss = document.querySelector("#pathloss");
+const ping = document.querySelector("#ping");
+const bandwidth = document.querySelector("#bandwidth");
+const speed = document.querySelector("#speed");
 const notes = document.querySelector("#notes");
 const confirm = document.querySelector("#confirm");
 const reset = document.querySelector("#reset");
 const radioStillDown = document.querySelector("#radioStillDown");
 const outageMode = document.querySelector("#outageMode");
 const inputs = document.getElementsByTagName("input");
-const ubiquitiButton = document.querySelector("#ubiquiti");
-const telradButton = document.querySelector("#telrad");
-const mimosaButton = document.querySelector("#mimosa");
-const cambiumButton = document.querySelector("#cambium");
-const taranaButton = document.querySelector("#tarana");
-const clipboardConfirm = document.getElementById("clipboardConfirm");
-let currentRadio;
 
 function findLable(e) {
   let idVal = e.id;
@@ -35,65 +37,13 @@ function checklistCreator() {
 }
 
 function createMessage() {
-  let info = `Complaint: ${complaint.value}\n\n`;
-  let currentLabel;
-  let currentInput;
-  let remoteSignal;
-  let currentId;
-  for (let m = 0; m < currentRadio.querySelectorAll("input").length; m++) {
-    currentInput = currentRadio.querySelectorAll("input")[m];
-    currentId = currentInput.id
-    if (currentInput.value) {
-      if(currentInput.id === "remoteSignal") {
-        remoteSignal = currentInput.value;
-        continue;
-      } else if (currentId.includes("Chains")) {
-        if(currentId.includes("remote")) {
-        info += `Remote Signal: -${remoteSignal} Δ${currentInput.value}\n`
-        continue;
-        } else {
-          info += ` Δ${currentInput.value}\n`
-          continue;
-        }
-      }
-      currentLabel = findLable(currentInput)
-      info += `${currentLabel}${(currentId.includes("signal") ||
-      currentId.includes("Signal") ||
-      currentId.includes("Noise")) ? " -" : " " }${currentInput.value}`;
-      switch(currentLabel) {
-        case "Ping:":
-          info += " ms"
-          break;
-        case "Bandwidth:":
-          info += " mbps"
-          break;
-        case "Pathloss:":
-          info += "dB"
-          break;
-        case "Radio Frequency:":
-        case "Router Frequency:": 
-          info += " MHz"
-          break;
-        default:
-          break;
-      }
-	if(currentLabel === "Local Signal:") {
-       continue;
-      }
-      info += "\n"
-    }
-  }
-  info += "\n";
+  let info = `Complaint: ${complaint.value}\n\nConnected AP: ${connectedAp.value}\nOperator ID: ${operatorId.value}\nSINR: ${sinrDown.value} / ${sinrUp.value}\nAlignment Metric: ${alignmentMetric.value}\nPathloss: ${pathloss.value} dB\nPing: ${ping.value} ms\nBandwidth: ${bandwidth.value} Mbps\nLAN Speed: ${speed.value}\n\n`;
   const checklist = checklistCreator();
   const notesValue = "\nNotes:\n\n" + notes.value;
-  if (radioStillDown.checked || outageMode.checked) {
-    info = `Complaint: ${complaint.value}\n\n`;
+  if(radioStillDown.checked || outageMode.checked) {
+    info = `Complaint: ${complaint.value}\n\n`
   }
-  let radioName = currentRadio.id;
-  radioName = radioName.split("");
-  radioName[0] = radioName[0].toUpperCase();
-  radioName = radioName.join("");
-  const completeMessage = `${radioName.replace("Radio", "")}\n\n` + info + checklist + notesValue;
+  const completeMessage = "Tarana\n\n" + info + checklist + notesValue;
   document.getElementById("clipboardConfirm").style.display = "flex";
   setTimeout(
     () => (document.getElementById("clipboardConfirm").style.display = "none"),
@@ -106,7 +56,7 @@ const resetPage = () => {
   for (let k = 0; k < inputs.length; k++) {
     inputs[k].value = "";
     if (inputs[k].getAttribute("type") === "checkbox") {
-      if (inputs[k].id === "outageMode") {
+      if(inputs[k].id === "outageMode") {
         continue;
       }
       inputs[k].checked = false;
@@ -114,12 +64,12 @@ const resetPage = () => {
   }
   document.querySelector("textarea").value = "";
   disable();
-};
+}
 
 function disable() {
   const disableBool = radioStillDown.checked || outageMode.checked;
   for (let l = 0; l < inputs.length; l++) {
-    if (l === 0) {
+    if(l === 0) {
       continue;
     }
     if (inputs[l].getAttribute("type") === "text") {
@@ -128,61 +78,7 @@ function disable() {
   }
 }
 
-function showInfoContainers(radio, e = {target: {id: "ubiquiti"}}) {
-  const radios = document.querySelectorAll(".radioInfo");
-  const radioButtons = document.querySelectorAll("button");
-  let currentColor;
-  for (let n = 0; n < radios.length; n++) {
-    radios[n].style.display = "none";
-  }
-  currentRadio = document.getElementById(radio);
-  currentRadio.style.display = "grid";
-  switch(e.target.id) {
-    case "ubiquiti":
-      currentColor = "#2a81fa";
-      break;
-    case "telrad":
-      currentColor = "#bbc3fa";
-      break;
-    case "mimosa":
-      currentColor = "#fa8e1b";
-      break;
-    case "cambium":
-      currentColor = "#4c28fc";
-      break;
-    case "tarana":
-      currentColor = "#239664";
-      break;
-    default:
-      currentColor = "#212121";
-      break;
-  }
-  document.documentElement.style.setProperty("--color", currentColor)
-  for (let b = 0; b < radioButtons.length; b++) {
-    if (document.getElementById(radioButtons[b].id).classList.contains("selected")) {
-      document.getElementById(radioButtons[b].id).classList.toggle("selected")
-    }
-    if(radioButtons[b].id === e.target.id) {
-      document.getElementById(radioButtons[b].id).classList.toggle("selected");
-      continue;
-    }  
-  }
-}
-
-clipboardConfirm.addEventListener("click", () => {
-  clipboardConfirm.style.display = "none";
-});
 confirm.addEventListener("click", createMessage);
 reset.addEventListener("click", resetPage);
 radioStillDown.addEventListener("click", disable);
 outageMode.addEventListener("click", disable);
-ubiquitiButton.addEventListener("click", (e) =>
-  showInfoContainers("ubiquitiRadio", e)
-);
-telradButton.addEventListener("click", (e) => showInfoContainers("telradRadio", e));
-mimosaButton.addEventListener("click", (e) => showInfoContainers("mimosaRadio", e));
-cambiumButton.addEventListener("click", (e) =>
-  showInfoContainers("cambiumRadio", e)
-);
-taranaButton.addEventListener("click", (e) => showInfoContainers("taranaRadio", e));
-showInfoContainers("ubiquitiRadio")
